8df9fec0dc81a525d035359d707e720d7a6f43b2
HA <akhterhassan89@gmail.com>
2026-02-19 06:45:20 +0500

    upkeep

--- Lab04/1.s
+++ Lab04/1.s
@@ -1,2 +1,37 @@
 .text
-.globl main:
+.globl main
+
+main:
+    addi x10, x0, 5
+    jal  x1, fact
+    j exit    
+
+fact:
+    addi sp, sp, -16   # push stack
+    sw   x1, 8(sp)     # return address on stack
+    sw   x10, 0(sp)    
+
+    # BASE CASE
+    addi x5, x0, 1      # x5 = 1
+    blt  x10, x5, base  # If n < 1, base case
+
+    # recusrive else
+    addi x10, x10, -1 # n = n - 1
+    jal  x1, fact # fact(n-1)
+    
+    # returning to after the recursive call
+    lw   x6, 0(sp)       # load n to x6
+    mul  x10, x10, x6    # x10 = fact(n-1) * n
+    j    return
+
+base:
+    addi x10, x0, 1   # return 1 for base case
+
+return:
+    # release stack
+    lw   x1, 8(sp)       # Restore the next return address
+    addi sp, sp, 16      # release current block of stack calls
+    jalr x0, 0(x1)       # return to next address
+
+exit:
+    j exit
---
+++ Lab04/3.s
@@ -0,0 +1,59 @@
+.text
+.globl main
+
+main:
+ li x8, 0x100 # base address
+# initialzing the array values
+ li x20, 23
+ li x21, 12
+ li x22, 5
+ li x23, 44
+ li x24, 98
+ li x25, 53
+ li x26, 6
+ li x27, 89
+ li x28, 32
+ li x29, 65
+
+ sw x20, 0(x8)
+ sw x21, 4(x8)
+ sw x22, 8(x8)
+ sw x23, 12(x8)
+ sw x24, 16(x8)
+ sw x25, 20(x8)
+ sw x26, 24(x8)
+ sw x27, 28(x8)
+ sw x28, 32(x8)
+ sw x29, 36(x8)
+
+ li x9, 10 # size = 10
+
+outer_loop:
+ li x5, 0 # swapped = false
+ li x6, 1 # index = 1
+
+inner_loop:
+ bge x6, x9, end_inner # if index > 10, exit loop
+
+ slli x7, x6, 2 # shift left for arr offset
+ add x10, x8, x7 # calculated address
+
+ lw x11, 0(x10) # c[i]
+ lw x12, -4(x10) # c[i-1]
+
+ ble x12, x11, no_swap # if i-1 < i, dont swap
+ #-else
+ sw x12, 0(x10) #i = i-1 
+ sw x11, -4(x10)#i-1 = i
+
+ li x5, 1 # swapped = true
+
+no_swap:
+ addi x6, x6, 1 # i++
+ j inner_loop #loopback
+
+end_inner:
+ bnez x5, outer_loop
+
+exit:
+    j exit
