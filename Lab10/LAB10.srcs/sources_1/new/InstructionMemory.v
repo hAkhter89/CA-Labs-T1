4ae3eb226d9f0bf560223dd29fe5f2a9d5409e77
Hassan Akhter <ha10609@st.habib.edu.pk>
2026-04-11 12:14:05 +0500

    lab-10-almost-complete

---
+++ Lab10/LAB10.srcs/sources_1/new/InstructionMemory.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/11/2026 11:22:13 AM
+// Design Name: 
+// Module Name: InstructionMemory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instructionMemory#(
+    parameter OPERAND_LENGTH = 31
+    )(
+    input [OPERAND_LENGTH:0] instAddress,
+    output reg [31:0] instruction
+);
+
+reg [7:0] memory [0:255];
+
+    always @(*) begin
+        instruction = { memory[instAddress],
+                        memory[instAddress + 1],
+                        memory[instAddress + 2],
+                        memory[instAddress + 3] };
+    end
+
+    // Initialize memory
+    initial begin
+        // Load from external file
+        $readmemh("instructions.mem", memory);
+    end
+
+
+endmodule
